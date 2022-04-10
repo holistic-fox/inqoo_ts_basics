87d2991aaa46209ce9a220f97fb8391eeaccc166
Michał Witek <michal.witek.katowice@gmail.com>
2022-04-10 13:54:14 +0200

    add generic example

---
+++ live-examples/generic.ts
@@ -0,0 +1,14 @@
+import { people } from './sample_data/people';
+import { starships } from  './sample_data/starships';
+
+const random = () => Math.floor(Math.random()*10000);
+
+const generateIdsForArrayElems = <T>(array: T[]): (T & {id: number})[] => {
+    return array.map(item => ({id: random(), ...item}));
+}
+
+console.log(generateIdsForArrayElems(people));
+console.log(generateIdsForArrayElems(starships));
+
+//HOMEWORK
+// Make sure random generated ids are uniq
