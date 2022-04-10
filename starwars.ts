63c93f1552412954cd844a359b9881dc9787244a
Michał Witek <michal.witek.katowice@gmail.com>
2022-04-10 12:31:24 +0200

    class extend and implement interface

--- starwars.ts
+++ starwars.ts
@@ -1,5 +1,24 @@
 import { people } from './sample_data/people';
 import { starships } from  './sample_data/starships';
+import { IPeople } from './interfaces/IPeople';
+import { IStarships } from './interfaces/IStarships';
 
-console.log('people', people);
-console.log('starships', starships);
+abstract class StarWarsData{
+    protected _people: IPeople[];
+    protected _starships: IStarships[];
+
+    constructor(people: IPeople[], starships: IStarships[]) {
+        this._people = people;
+        this._starships = starships;
+    }
+}
+
+interface IStealAShip{
+    stealShip: () => {}
+}
+
+class StarWarsPerson extends StarWarsData implements IStealAShip{
+    public stealShip: () => {};
+}
+
+const starWarsPerson: StarWarsPerson = new StarWarsPerson(people, starships);
